48cd5132ac1bbe1dd3725b3d66591dcd80ec0f2b
nimblechapps <kirit@nimblechapps.com>
2017-03-01 16:07:04 +0530

    Localisation
    Localisation

---
+++ Localization-Demo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Localization-Demo
+//
+//  Created by Nimble Chapps on 16/09/16.
+//  Copyright © 2016 iOSDevCenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var labelTitle: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
